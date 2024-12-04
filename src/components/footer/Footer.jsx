8cca76269b957c8b39776712a3a2fb925f53abad
eqramanwar-01 <eqra1004@gmail.com>
2024-12-04 02:47:35 -0800

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer(){
+    return(
+        <div className = "footer-content">
+           &copy; 2025 All Right Reserved . By HTML Design
+        </div>
+    )
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header(){
+    return(
+        <div className = "header-tittle header-font">
+            <h1>My react App</h1>
+        </div>
+    )
+}
